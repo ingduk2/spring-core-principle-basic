419b257aebbe7dfe711cf1fcf16f897bc41a6ac6
ingduk2 <ingduk2@naver.com>
2020-09-23 04:22:57 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    //추상화 의존, 구체화 의존. --> DIP 위반 변경시 문
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
